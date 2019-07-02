ddc2e32b848bf23ee729d07c44e700ca5a8df5bf
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-07-02 21:32:06 +0900

    Update hoge

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Hoge from '@/components/hoge'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/hoge',
+      name: 'Hoge',
+      component: Hoge
     }
   ]
 })
